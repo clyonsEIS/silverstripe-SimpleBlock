d16e8f845517df16726dd02d3292600bf1e6aab1
helpfulrobot <chris@silverstripe.com>
2016-01-01 01:29:00 +1300

    Converted to PSR-2

--- code/SimpleBlock.php
+++ code/SimpleBlock.php
@@ -2,31 +2,31 @@
 
 class SimpleBlock extends DataObject
 {
-	private static $db = array(
-		'Title' => 'Varchar(255)',
-		'Active' => 'Boolean',
-		'Link' => 'Varchar(255)',
-		'isLinkExternal' => 'Boolean'
-	);
+    private static $db = array(
+        'Title' => 'Varchar(255)',
+        'Active' => 'Boolean',
+        'Link' => 'Varchar(255)',
+        'isLinkExternal' => 'Boolean'
+    );
 
-	private static $has_one = array(
-		'Image' => 'Image'
-	);
+    private static $has_one = array(
+        'Image' => 'Image'
+    );
 
-	private static $defaults = array('Active' => 1);
+    private static $defaults = array('Active' => 1);
 
-	private static $summary_fields = array('Title', 'Active');
+    private static $summary_fields = array('Title', 'Active');
 
-	public function getCMSFields()
-	{
-		$fields = parent::getCMSFields();
-		$fields->dataFieldByName('Link')->setRightTitle('Fully qualified (http://www.google.com) or internal (section/page)');
-		$fields->dataFieldByName('isLinkExternal')->setTitle('Open link in new window?');
-		return $fields;
-	}
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        $fields->dataFieldByName('Link')->setRightTitle('Fully qualified (http://www.google.com) or internal (section/page)');
+        $fields->dataFieldByName('isLinkExternal')->setTitle('Open link in new window?');
+        return $fields;
+    }
 
-	public function Target()
-	{
-		return $this->isLinkExternal ? '_blank' : '_self';
-	}
+    public function Target()
+    {
+        return $this->isLinkExternal ? '_blank' : '_self';
+    }
 }
